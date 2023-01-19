13a793417bd58b19a07f21d8c804362a921368f0
Scorpion645 <scorpion645@gmail.com>
2023-01-18 21:00:02 -0500

    very basic login screen initiated

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../constants/constants.dart';
+
+class LogInScreen extends StatelessWidget {
+  static String id = 'login screen';
+  const LogInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kMainColor,
+    );
+  }
+}

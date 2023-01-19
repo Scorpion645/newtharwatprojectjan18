8bd1221ffb6a797203ef3c43614efebd61fc4cfb
Scorpion645 <scorpion645@gmail.com>
2023-01-18 21:04:14 -0500

    login screen logo created

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,6 +10,25 @@ class LogInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kMainColor,
+      body: ListView(
+        children: [
+          Stack(alignment: Alignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(50),
+                child: Image.asset('assets/images/icons8-buy-100.png'),
+              ),
+              Positioned(
+                  bottom: 0,
+                  child: const Text('Buy it',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                          fontFamily: 'pacifico'))),
+            ],
+          )
+        ],
+      ),
     );
   }
 }

abdf086267eb45306ad7675f3c7770c368e7a4b1
Scorpion645 <scorpion645@gmail.com>
2023-01-24 22:49:45 -0500

    branch82,validation,auth,progress,provider,-first-attempt-complete-by-me

---
+++ lib/firebase_auth/Auth.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Auth {
+  final _auth = FirebaseAuth.instance;
+
+  signup(_email, _password) {
+    return _auth.createUserWithEmailAndPassword(
+        email: _email, password: _password);
+  }
+
+   login(_email, _password) {
+    return _auth.signInWithEmailAndPassword(
+        email: _email, password: _password);
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,93 +1,127 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:provider/provider.dart';
 
 import '../constants/constants.dart';
+import '../firebase_auth/Auth.dart';
+import '../provider/modal_hud.dart';
 import '../widgets/customButton.dart';
 import '../widgets/customTextField.dart';
 import 'signup_screen.dart';
 
 class LogInScreen extends StatelessWidget {
   static String id = 'login screen';
-   String _email = '', _password = '';
-   LogInScreen({super.key});
+  String _email = '', _password = '';
+  GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
+  LogInScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kMainColor,
-      body: ListView(
-        children: [
-          Stack(alignment: Alignment.center,
+      body: ModalProgressHUD(
+        inAsyncCall: Provider.of<ModalHud>(context).isLoading,
+        child: Form(
+          key: _globalKey,
+          child: ListView(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(50),
-                child: Image.asset('assets/images/icons8-buy-100.png'),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(50),
+                    child: Image.asset('assets/images/icons8-buy-100.png'),
+                  ),
+                  Positioned(
+                      bottom: 0,
+                      child: const Text('Buy it',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                              fontFamily: 'pacifico'))),
+                ],
               ),
-              Positioned(
-                  bottom: 0,
-                  child: const Text('Buy it',
+              SizedBox(
+                height: 20,
+              ),
+              CustomTextField(
+                myHint: 'Enter your Email',
+                myIcon: Icons.email,
+                onClick: (String value) {
+                  _email = value;
+                },
+                myObscuredText: false,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              CustomTextField(
+                myHint: 'Enter your password',
+                myIcon: Icons.lock,
+                onClick: (value) {
+                  _password = value;
+                },
+                myObscuredText: true,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 79,
+              ),
+              CustomButton(
+                buttonTitle: 'Log in',
+                onClick: () async {
+                  Provider.of<ModalHud>(context, listen: false)
+                      .changeIsLoading(true);
+                  if (_globalKey.currentState!.validate()) {
+                    try {
+                      await Auth().login(_email, _password);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text(
+                        'Login successful!',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      )));
+                      Provider.of<ModalHud>(context, listen: false)
+                      .changeIsLoading(false);
+                    } on FirebaseException catch (e) {
+                      Provider.of<ModalHud>(context, listen: false)
+                      .changeIsLoading(false);
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(e.message!)));
+                    }
+                  }
+                  Provider.of<ModalHud>(context, listen: false)
+                      .changeIsLoading(false);
+                  print(_email);
+                  print(_password);
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Do not have an account?',
                       style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25,
-                          fontFamily: 'pacifico'))),
+                          fontWeight: FontWeight.normal, fontSize: 14)),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.popAndPushNamed(context, SignUpScreen.id);
+                      },
+                      child: Text('Sign up',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.black)))
+                ],
+              )
             ],
           ),
-          SizedBox(
-            height: 20,
-          ),
-          CustomTextField(
-            myHint: 'Enter your Email',
-            myIcon: Icons.email,
-            onClick: (String value) {
-              _email = value;
-            },
-            myObscuredText: false,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          CustomTextField(
-            myHint: 'Enter your password',
-            myIcon: Icons.lock,
-            onClick: (value) {
-              _password = value;
-            },
-            myObscuredText: true,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          
-          SizedBox(
-            height: 79,
-          ),
-          CustomButton(
-            buttonTitle: 'Log in',
-            onClick: () {
-              print(_email);
-              print(_password);
-            },
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Do not have an account?',
-                  style:
-                      TextStyle(fontWeight: FontWeight.normal, fontSize: 14)),
-              TextButton(
-                  onPressed: () {
-                    Navigator.popAndPushNamed(context, SignUpScreen.id);
-                  },
-                  child: Text('Sign up',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.black)))
-            ],
-          )
-        ],
+        ),
       ),
     );
   }

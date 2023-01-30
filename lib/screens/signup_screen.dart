7bf40f7daafeedd4eaa4e8a4043fdbba22e447cd
Scorpion645 <scorpion645@gmail.com>
2023-01-30 18:56:36 -0500

    initial custom textfield created

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -16,10 +16,45 @@ class SignUpScreen extends StatelessWidget {
             SizedBox(
               height: 40,
             ),
-            CustomLogo()
+            CustomLogo(),
+            SizedBox(height: 20,),
+            Custom_TextField(),
           ],
         ));
   }
 }
 
+class Custom_TextField extends StatelessWidget {
+  const Custom_TextField({
+    super.key,
+  });
 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        keyboardType: TextInputType.name,
+        onChanged: (value) {},
+        obscureText: false,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Colors.white, width: 3)),
+          filled: true,
+          fillColor: kSecondaryColor,
+          border: InputBorder.none,
+          prefixIcon: Icon(
+            Icons.person,
+            color: Colors.black,
+          ),
+          hintText: 'Enter your Name',
+          hintStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: Colors.grey[600]),
+        ),
+      ),
+    );
+  }
+}

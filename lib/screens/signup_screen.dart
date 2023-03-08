80e9eef3625fbe9276e4f3b081c47e0e26dc9302
Scorpion645 <scorpion645@gmail.com>
2023-03-07 22:46:55 -0500

    Logo created

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -10,25 +10,6 @@ class SignUpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kMainColor,
-      body: ListView(
-        children: [
-          Stack(alignment: Alignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(50),
-                child: Image.asset('assets/images/icons8-buy-100.png'),
-              ),
-              Positioned(
-                  bottom: 0,
-                  child: const Text('Buy it',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25,
-                          fontFamily: 'pacifico'))),
-            ],
-          )
-        ],
-      ),
     );
   }
 }

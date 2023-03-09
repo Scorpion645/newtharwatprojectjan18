a2e4880b06984cd1e4291e1061daf9acd8daf2e4
Scorpion645 <scorpion645@gmail.com>
2023-03-08 23:14:52 -0500

    main admin screen created

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: LoginScreen.id,
+      initialRoute: MainAdminScreen.id,
       routes: {
         SignUpScreen.id: (context) => SignUpScreen(),
         LoginScreen.id: (context) => LoginScreen(),
--- lib/screens/admin screens/main_admin_screen.dart
+++ lib/screens/admin screens/main_admin_screen.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/constants.dart';
+import '../../widgets/custom_Button.dart';
+
 class MainAdminScreen extends StatelessWidget {
   static String id = 'main Admin screen';
-const MainAdminScreen ({Key? key}) : super(key: key);
+  const MainAdminScreen({Key? key}) : super(key: key);
 
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-body: Center(child: Text('Admin Screen')),
-
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kMainColor,
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 220,
+          ),
+          CustomButton(butTitle: 'Add product'),
+          SizedBox(
+            height: 20,
+          ),
+          CustomButton(butTitle: 'Edit product'),
+          SizedBox(
+            height: 20,
+          ),
+          CustomButton(butTitle: 'Order review'),
+        ],
+      ),
+    );
+  }
 }

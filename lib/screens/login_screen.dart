294f917a923a3b5b1fdb945091853c7c06d04756
Scorpion645 <scorpion645@gmail.com>
2023-03-16 20:07:56 -0400

    validation complete

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../constants/constants.dart';
@@ -10,9 +11,9 @@ import 'signup_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   static String id = 'log in screen';
-   LoginScreen({Key? key}) : super(key: key);
-    GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
-
+  late String _email, _password;
+  LoginScreen({Key? key}) : super(key: key);
+  GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -28,20 +29,57 @@ class LoginScreen extends StatelessWidget {
             CustomTextField(
               myIcon: Icons.email,
               myHint: 'Enter your Email',
+              onClick: (value) {
+                _email = value!;
+              },
             ),
             heightSizedbox(20),
             CustomTextField(
               myIcon: Icons.lock,
               myHint: 'Enter your Password',
               myObscured: true,
+              onClick: (value) {
+                _password = value!;
+              },
             ),
             heightSizedbox(99),
             CustomButton(
-              butTitle: 'Log in',
-              onClick: () {
-                _globalKey.currentState!.validate();
-              },
-            ),
+                butTitle: 'Log in',
+                onClick: () async {
+                  _globalKey.currentState!.save();
+
+                  print(_email);
+                  print(_password);
+                  if (_globalKey.currentState!.validate()) {
+                    print('Hello');
+
+                    print(_email);
+                    try {
+                      await FirebaseAuth.instance.signInWithEmailAndPassword(
+                          email: _email, password: _password);
+
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text(
+                        'Admin logged in successfully',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.center,
+                      )));
+                    } on FirebaseException catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text(
+                        e.message!,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.center,
+                      )));
+                    }
+                  }
+                }),
             CustomRow(
               firstText: 'Already have an account?',
               secondText: 'Signup',
